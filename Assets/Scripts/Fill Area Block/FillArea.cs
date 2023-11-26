21f411d62f49dc21e8d51db868754fbda9de6c94
Utku Gülcü <utkugulcu@gmail.com>
2023-11-26 23:55:23 +0300

    Demo Finished

--- Assets/Scripts/Fill Area Block/FillArea.cs
+++ Assets/Scripts/Fill Area Block/FillArea.cs
@@ -15,6 +15,7 @@ public class FillArea : MonoBehaviour
     [Header("References")]
     [SerializeField] private FillAreaUI fillAreaUI;
     [SerializeField] private FillAreaAnimation fillAreaAnimation;
+    [SerializeField] private Collider levelGroundCollider;
 
     [Header("Events")]
     [SerializeField] private GameEventSO OnLevelFailed;
@@ -24,11 +25,13 @@ public class FillArea : MonoBehaviour
 
     private int requiredCollectables;
     private int collectableCount;
+    private List<Collider> collectableColliderList = new List<Collider>();
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Collectable collectable))
         {
+            collectableColliderList.Add(collectable.GetComponent<Collider>());
             collectableCount++;
             UpdateText();
         }
@@ -53,6 +56,8 @@ public class FillArea : MonoBehaviour
         float waitTime = 1.5f;
         yield return Helpers.GetWait(waitTime);
         
+        IgnoreCollisionWithCollectables();
+        
         if (collectableCount >= requiredCollectables)
         {
             fillAreaAnimation.PlayWinAnimation();
@@ -73,5 +78,23 @@ public class FillArea : MonoBehaviour
         LevelSO levelSO = LevelManager.Instance.GetCurrentLevel();
         requiredCollectables = levelSO.GetFillAreaRequirement(type);
         fillAreaUI.SetupRequiredCollectableText(requiredCollectables);
+    }
+
+    private void IgnoreCollisionWithCollectables()
+    {
+        if (collectableColliderList.Count == 0)
+        {
+            return;
+        }
+        
+        foreach (Collider collectableCollider in collectableColliderList)
+        {
+            if (collectableCollider == null)
+            {
+                return;
+            }
+            
+            Physics.IgnoreCollision(levelGroundCollider, collectableCollider);
+        }
     }
 }
